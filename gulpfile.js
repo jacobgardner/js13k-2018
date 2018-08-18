a9f00b93dc3db5b4efe8054960c9aba1a0011f2f
Jacob Gardner <jacob.v.gardner@gmail.com>
2018-08-18 17:29:16 -0500

    Fresh start

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+const sequence = require("gulp-sequence");
+
+require('./tasks');
+
+gulp.task("build", (cb) => {
+    sequence(["build-shaders", "build-audio"], "build-scripts", "reload")(cb);
+});
+
+gulp.task("reload", () => {
+    browserSync.reload();
+});
+
+gulp.task("browserSync-init", () => {
+    return browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+gulp.task("watch-js", ["build"], () => {
+    return gulp.watch(
+        ["src/**/*", "assets/**/*", "!src/shaders/**/*", "!src/music/**/*"],
+        ["build"]
+    );
+});
+
+gulp.task("watch", ["watch-js"]);
+gulp.task("serve", sequence("browserSync-init", "watch"));
+gulp.task("default", ["serve"]);
